c72ce73f547c894743c0f3cea4587f01b84a1e18
hai che viet <cheviethai123@gmail.com>
2022-07-11 09:47:55 +0700

    update log dir

--- mlops-best-practices/app/logger.py
+++ mlops-best-practices/app/logger.py
@@ -1,6 +1,8 @@
 import os
 
 LOGGER_DIR = os.getenv("LOGGER_DIR", "logs")
+if not os.path.exists(LOGGER_DIR):
+    os.makedirs(LOGGER_DIR)
 level_log = "DEBUG"
 format_log = (
     "%(asctime)s [%(threadName)-12.12s] "
@@ -19,8 +21,21 @@ DEFAULT_LOGGER = {
             "class": "logging.StreamHandler",
             "formatter": "standard",
         },
+        "model_log": {
+            "level": level_log,
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": "{}/{}".format(LOGGER_DIR, "model_log.log"),
+            "formatter": "standard",
+            "when": "d",
+            "interval": 1,
+            "backupCount": 5,
+        },
     },
     "loggers": {
         "app.main": {"handlers": ["api"], "level": level_log},
+        "app.core": {
+            "handlers": ["model_log"],
+            "level": level_log,
+        },
     },
 }
